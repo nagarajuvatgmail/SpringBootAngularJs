9988db0bea491db8841b33f7e966829d1b0bb80d
NagarajuVeeranala <raju.veeranala@gmail.com>
2017-08-12 00:19:41 -0400

    Added pagination to the Table

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngRoute','ngResource']);
+var app = angular.module('app', ['ngRoute','ngResource','angularUtils.directives.dirPagination']);
 app.config(function($routeProvider){
     $routeProvider
         .when('/users',{
